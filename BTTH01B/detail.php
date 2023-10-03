fd57261bccddc5793fef85df2bc6bcb0cc9b7921
dungne <145663175+dung2302@users.noreply.github.com>
2023-10-03 18:33:44 +0700

    Update detail.php

--- BTTH01B/detail.php
+++ BTTH01B/detail.php
@@ -1,12 +1,35 @@
+<?php 
+    try {
+        $conn = new PDO("mysql:host=localhost:4306;dbname=btth01_cse485", "root","");
+
+        $sql = "SELECT * FROM baiviet ORDER BY ma_bviet";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+    } catch(PDOException $e) {
+        echo "Error: ".$e->getMessage();
+    }
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MUSIC</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 <style>
     a{
         text-decoration: none;
@@ -17,17 +40,26 @@
         include "layout/header.php";
     ?>
     <div class="container-fluit">
-        <div class="row">
-            <div class="col-2 mt-4">
-                <img src="img/anh3.jpg" alt="anh3">
-            </div>
-            <div class="col-7 mt-4">
-                
-            </div>
+    <div class="card-body">
+        <?php
+        // Hiển thị thông tin của bài hát
+        if ($baiHat) {?>
+            <h5><?= $baiHat['tieude']?></h5>
+            <p><b>Bài hát: </b><?= $baiHat['ten_bhat']?></p>
+            <p><b>Thể loại: </b><?= $tenTheLoai['ten_tloai']?></p>
+            <p><b>Tóm tắt: </b><?=$baiHat['tomtat']?></p>
+            <p><b>Nội dung: </b><?=$baiHat['noidung']?></p>
+            <p><b>Tác giả: </b><?=$tenTacGia['ten_tgia']?></p>
+        <?php } else {
+            echo "Không tìm thấy bài hát.";
+        }
+        ?>
         </div>
     </div>
     <?php 
         include "layout/footer.php";
     ?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 </html>
